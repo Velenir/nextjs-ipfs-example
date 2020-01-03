3d1c389c5934a13675f7d8650b72143908b040e5
Velenir <velenir@gmail.com>
2020-01-03 14:16:22 +0300

    add BaseLink component

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import Link from 'next/link'
+import {resolve} from 'url'
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
@@ -9,13 +10,47 @@ const links = [
   return link
 })
 
+const BaseLink = ({href, as, ...rest}) => {
+
+  const newAs = useMemo(() => {
+    let baseURI_as = as || href
+  
+    // make absolute url relative
+    // when displayed in url bar
+    if (baseURI_as.startsWith('/')) {
+      //  for static html compilation
+      baseURI_as = '.' + href
+      // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
+  
+      // on the client
+      if (typeof document !== 'undefined') {
+        baseURI_as = resolve(document.baseURI, baseURI_as)
+        // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
+      }
+    }
+    return baseURIAs
+  }, [as, href])
+
+  return <Link {...rest} href={href} as={newAs} />
+}
+
 const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href="/">
+        <BaseLink href="/">
           <a>Home</a>
-        </Link>
+        </BaseLink>
+      </li>
+      <li>
+        <BaseLink href="/about">
+          <a>About</a>
+        </BaseLink>
+      </li>
+      <li>
+        <BaseLink href="/deep/p">
+          <a>Deep Page</a>
+        </BaseLink>
       </li>
       {links.map(({ key, href, label }) => (
         <li key={key}>
